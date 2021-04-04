9ed40786575092aa18842a2c64bb683e887865e0
ping <chenpingping1234@qq.com>
2021-04-04 15:29:42 +0800

    ceshi1

---
+++ s-registry/src/main/java/com/sakura/LyRegistry.java
@@ -0,0 +1,18 @@
+package com.sakura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author chenpingping
+ * @version 1.0
+ * @date 2021/4/4 12:55
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LyRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(LyRegistry.class, args);
+    }
+}
