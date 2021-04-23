6279466e4a6f5b0864d37760619312344b15459c
ping <chenpingping1234@qq.com>
2021-04-24 01:10:02 +0800

    ceshi11

--- s-item/s-item-service/src/main/java/com/sakura/item/mapper/CategoryMapper.java
+++ s-item/s-item-service/src/main/java/com/sakura/item/mapper/CategoryMapper.java
@@ -1,7 +1,10 @@
 package com.sakura.item.mapper;
 
 import com.sakura.item.po.Category;
+import org.apache.ibatis.annotations.Select;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 /**
  * @author chenpingping
@@ -9,5 +12,11 @@ import tk.mybatis.mapper.common.Mapper;
  * @date 2021/4/7 11:35
  */
 public interface CategoryMapper extends Mapper<Category> {
-
+    /**
+     * 根据品牌id查询商品分类
+     * @param bid
+     * @return
+     */
+    @Select("SELECT * FROM tb_category WHERE id IN (SELECT category_id FROM tb_category_brand WHERE brand_id = #{bid})")
+    List<Category> queryByBrandId(Long bid);
 }
